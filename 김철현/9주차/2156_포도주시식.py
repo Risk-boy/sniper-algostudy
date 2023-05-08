f88baeae5a5b0ad9212930bf5768b885dd6f600a
hyeony <fogvhdooz@gmail.com>
2023-05-08 13:10:51 +0900

    solve week9 algo

---
+++ 김철현/9주차/2156_포도주시식.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open("input.txt")
+
+
+n = int(input())
+
+arr = [0]
+
+for _ in range(n):
+    arr.append(int(input()))
+
+dp = [0] * (n+1)
+
+# n이 2이하인 경우 runtime error 발생
+if n >= 1:
+    dp[1] = arr[1]
+if n >= 2:
+    dp[2] = arr[1] + arr[2]
+
+# O O X / O X O / X O O
+for i in range(3, n+1):
+    dp[i] = max(dp[i-1], dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i])
+
+print(max(dp))
