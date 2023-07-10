d233a3c25367278ce98d162df2f5051d4f3b5ac8
Aiden-Jung <hyunseok0314@gmail.com>
2023-07-10 16:35:57 +0900

    week 18

--- 정현석/18주차/1316.py
+++ 정현석/18주차/1316.py
@@ -1,16 +1,25 @@
 import sys
 
-[n, m] = map(int, sys.stdin.readline().rstrip().split())
-
-strings = set()
-
-for _ in range(n):
-    strings.add(sys.stdin.readline().rstrip())
+n = int(sys.stdin.readline().rstrip())
 
 count = 0
 
-for _ in range(m):
-    if sys.stdin.readline().rstrip() in strings:
+for _ in range(n):
+    word = list(sys.stdin.readline().rstrip())
+    
+    flag = True
+    
+    letters = set()
+    prev = None
+    for i in range(len(word)):
+        if prev != word[i]:
+            if word[i] not in letters:
+                letters.add(word[i])
+            else:
+                flag = False
+                break
+        prev = word[i]
+    if flag:
         count += 1
-        
+    
 print(count)
